c6d8ef6fc0cc310ce3e838f63efcdf3399c36aa2
haileygillespie <hailey0101gillespie@gmail.com>
2025-11-04 10:39:16 -0600

    added mongoose

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,21 @@
-import { dbConnectionStatus } from "@/db/connection-status";
-import clientPromise from "@/lib/mongodb";
+import connectDB from "@/lib/mongoose";
+import User from "@/models/User";
 
 export default async function Home() {
-  const result = await dbConnectionStatus();
-  const client = await clientPromise;
-  const mongo = client.db('sample_mflix')
-  const users = await mongo.collection('users').find({}).limit(10).toArray();
+  // Initialize MongoDB Connection with Mongoose
+  await connectDB();
+
+  // Get all user documents
+  const users = await User.find({})
+
   return (
     <div className="text-center">
       <h1>Pet Chart</h1>
-      <p>MongoDB Connection Status: {result}</p>
       <br />
-      <h2>First 10 Users Found:</h2>
+      <h2>Users Found:</h2>
       <ul>
         {users.map((user) => (
-          <li key={user._id.toString()}>{user.name} - {user.email}</li>
+          <li key={user._id}>{user.name}</li>
         ))}
       </ul>
     </div>
---
+++ models/User.ts
@@ -0,0 +1,15 @@
+import mongoose, { Schema, Document } from "mongoose";
+
+// Define TypeScript interface for a User
+export interface User extends Document {
+    name: string;
+}
+// Create Mongoose schema from interface
+const UserSchema = new Schema<User>(
+    {
+        name: { type: String, required: true }
+    }
+);
+
+// Prevent model recompilation in development
+export default mongoose.models.User || mongoose.model<User>("User", UserSchema);
