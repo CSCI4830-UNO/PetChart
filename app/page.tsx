b5d30ebb99f371bddd86eb5358fadee84f473260
haileygillespie <hailey0101gillespie@gmail.com>
2025-11-07 13:24:04 -0600

    added logic to create user doc on sign in

--- app/page.tsx
+++ app/page.tsx
@@ -13,6 +13,20 @@ export default async function Home() {
   // Get Google OAuth Session Info
   const session = await getServerSession();
 
+  // Create user on sign in if didn't already exist
+  if (session != null) {
+    // check if user exists with email
+    const foundUser = await User.find({email: session?.user?.email})
+    if (foundUser.length == 0) {
+      // if not, try to create user with email & name
+      try {
+        await User.create({name: session?.user?.name, email: session?.user?.email})
+      } catch (error) {
+        console.error("ERROR Creating User: ", error)
+      }
+    }
+  }
+
   return (
     <div className="text-center">
       <h1 className="mb-4 text-4xl font-extrabold">Pet Chart</h1>
@@ -21,7 +35,7 @@ export default async function Home() {
       <h2>Users Found:</h2>
       <ul>
         {users.map((user) => (
-          <li key={user._id}>{user.name}</li>
+          <li key={user._id}>{user.name}, {user.email}</li>
         ))}
       </ul>
     </div>
