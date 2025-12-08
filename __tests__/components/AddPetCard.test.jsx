7523767ecd2e86455479b04b96a435e774ebdd67
Hailey Gillespie <hailey0101gillespie@gmail.com>
2025-12-08 09:04:10 -0600

    Hailey unittest (#25)
    * Added Jest
    
    * added first test
    
    * added signInBtn tests
    
    * updated comments
    
    * added test
    
    * cleaned up comments

---
+++ __tests__/components/AddPetCard.test.jsx
@@ -0,0 +1,44 @@
+import '@testing-library/jest-dom'
+import { fireEvent, render, screen } from '@testing-library/react'
+import { AddPetCard } from '../../components/AddPetCard'
+
+// mock userRouter
+jest.mock('next/navigation', () => ({
+    useRouter: jest.fn(() => ({
+        push: jest.fn(),
+        replace: jest.fn(),
+        prefetch: jest.fn()
+        })),
+}));
+
+// mock userRouter.push()
+jest.mock('next/router', () => ({
+    useRouter: jest.fn(() => ({
+        push: jest.fn()
+    })),
+}));
+
+// mock the next-auth module
+jest.mock('next-auth/react', () => ({
+    signIn: jest.fn(),
+}));
+
+describe('AddPetCard', () => {
+    it('renders and calls correctly', () => {
+        render(<AddPetCard/>)
+
+        // check title displays
+        const titleElement = screen.getByText("Add New Pet")
+        expect(titleElement).toBeInTheDocument()
+
+        // make sure the button displays
+        const button = screen.getByRole('button')
+        expect(button).toBeInTheDocument()
+
+        // check button click
+        fireEvent.click(button); 
+    })
+})
+
+
+
---
+++ __tests__/components/signInBtn.test.jsx
@@ -0,0 +1,22 @@
+import '@testing-library/jest-dom'
+import { fireEvent, render, screen } from '@testing-library/react'
+import { SignInBtn } from '../../components/signInBtn'
+
+// Mock the next-auth module & sign in func
+jest.mock('next-auth/react', () => ({
+    signIn: jest.fn(),
+}));
+
+describe('SignInBtn', () => {
+    it('renders and calls correctly', () => {
+        render(<SignInBtn/>)
+
+        // make sure the button displays
+        const button = screen.getByRole('button')
+        expect(button).toBeInTheDocument()
+
+        // check sign in gets called, specifically next-auth/react.signIn()
+        fireEvent.click(button)
+        expect(require('next-auth/react').signIn).toHaveBeenCalledTimes(1);
+    })
+})
